a31205da46c4cc7c47dc877dff86c265621b8580
Francisco Andrade <francisco.andrade@unit-consulting.ae>
2020-03-21 15:29:05 +0000

    Add first version of imdb datasets handler

---
+++ imdb-datasets/lambda/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Contains the lambda handler
+"""
+
+import os
+
+import imdb_datasets_handler
+
+# pylint: disable=W0613
+def lambda_handler(event, context):
+    """
+    Lambda function to be called by the CW rule
+    """
+
+    handler = imdb_datasets_handler.ImdbDatasetsHandler()
+
+    handler.process()
